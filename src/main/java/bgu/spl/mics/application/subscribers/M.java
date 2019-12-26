49a7fb4a5e1545aded2089eb64682dec6d5b8fd6
yair.gur.arieh@gmail.com <Yy31415926535>
2019-12-26 22:46:35 +0200

    this is before changes of yair to find deadlocks

--- src/main/java/bgu/spl/mics/application/subscribers/M.java
+++ src/main/java/bgu/spl/mics/application/subscribers/M.java
@@ -4,12 +4,14 @@ import bgu.spl.mics.Future;
 import bgu.spl.mics.MessageBroker;
 import bgu.spl.mics.MessageBrokerImpl;
 import bgu.spl.mics.Subscriber;
+import bgu.spl.mics.application.ThreadCounter;
 import bgu.spl.mics.application.messages.*;
 import bgu.spl.mics.application.passiveObjects.Diary;
 import bgu.spl.mics.application.passiveObjects.MissionInfo;
 import bgu.spl.mics.application.passiveObjects.Report;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 //import java.util.concurrent.TimeUnit;
 
 /**
@@ -36,51 +38,63 @@ public class M extends Subscriber {
 	@Override
 	protected void initialize()
 	{
+		ThreadCounter.getInstance().increase();
+		mb.register(this); // is it needed?
+		System.out.println("M is init " + id);
 		Diary diary = Diary.getInstance();
 		subscribeBroadcast(TickBroadcast.class, (broadcast) -> {
-			//System.out.println("M- ,initialize method, before getting current tick " + currentTime);
+			//System.out.println("M- " + id + " ,initialize method, before getting current tick " + currentTime);
 			currentTime = broadcast.getCurrentTick();
 		});
+
 		subscribeEvent(MissionRecivedEvent.class, (event) ->{
 			diary.incrementTotal();
 			mInfo = new MissionInfo(event.getMissionName(), event.getSerialAgentsNumbers(), event.getGadget(), event.getTimeIssued(), event.getTimeExpired(), event.getDuration());
-			System.out.println("M- ,initialize method, after creating missionInfo: " + mInfo.getMissionName() );
+			System.out.println("M-" + id + " I got the mission " + mInfo.getMissionName() + " with agents " + mInfo.getSerialAgentsNumbers() + " issued in " + mInfo.getTimeIssued() + " in system time: " + currentTime);
 			List<String> SerialAgentsNumbers = event.getSerialAgentsNumbers();
 
 			AgentsAvailableEvent availableEvent = new AgentsAvailableEvent(SerialAgentsNumbers);
-
 			Future<Boolean> future1 = getSimplePublisher().sendEvent(availableEvent);
 
-			if(future1 != null & future1.get() == true){
-
-
-				// 8
+			if(future1 != null && future1.get(event.getTimeExpired()-currentTime, TimeUnit.MILLISECONDS) == true){
+				System.out.println("M-" + id + " There are available Agents, they are: " + availableEvent.getSerialAgentsNumbers() + "in the time " + currentTime);
 				GadgetAvailableEvent gadgetEvent = new GadgetAvailableEvent(event.getGadget());
+				System.out.println("M-" + id + " sent event for finding gadget: " + event.getGadget());
 				Future<Integer> future2 = getSimplePublisher().sendEvent(gadgetEvent);
 
-				if(future2.get() == null){ // there is a gadget, w continue
-					System.out.println("M-" + id + " , there is no gadget ,in ReleaseAgentsEvent . GADGET IS: " + event.getGadget() +" mission name: "+ event.getMissionName() + "serial numbers= " + event.getSerialAgentsNumbers());
-					ReleaseAgentsEvent ReleaseAgentsEvent = new ReleaseAgentsEvent(SerialAgentsNumbers);
-					Future<Boolean> future3 = getSimplePublisher().sendEvent(ReleaseAgentsEvent);
-				}
-				else{
-					System.out.println("M- " + id + " there is a gadget! yay!. gadget is: " + event.getGadget() +" mission name: "+ event.getMissionName() + "serial numbers= " + event.getSerialAgentsNumbers() );
-					if(future2.get() != null && event.getTimeExpired() >= currentTime){
+				if(future2.get() != null && event.getTimeExpired() >= currentTime){ // there is a gadget and TimeExpired bigger then currentTime. continue.
+
+					System.out.println("M- " + id + " There is a gadget! yay!. gadget is: " + event.getGadget() +" mission name: "+ event.getMissionName() + "serial numbers= " + event.getSerialAgentsNumbers() + " time issued is " + mInfo.getTimeIssued() + " and current time is: " + currentTime);
+					if(event.getTimeExpired() >= currentTime + event.getDuration()){
 						SendAgentsEvent sendAgentsEvent = new SendAgentsEvent(mInfo.getDuration(), SerialAgentsNumbers);
 						Future<toReturn> futureSendAgents = getSimplePublisher().sendEvent(sendAgentsEvent);
 						if(futureSendAgents.get()!=null){
+							System.out.println("M-" + id + " just sent Agents " + event.getSerialAgentsNumbers() + " and now they are not available");
+
 							Report rep = new Report(mInfo.getMissionName(), id, futureSendAgents.get().getId(), mInfo.getSerialAgentsNumbers(), futureSendAgents.get().getLs(), mInfo.getGadget(), mInfo.getTimeIssued(), future2.get().intValue(), (int)currentTime);
 							diary.addReport(rep);
 							System.out.println(rep.toString());
+
 						}
+
 					}
 				}
+				else{ // there is no gadget available
+					System.out.println("M-" + id + " There is no gadget. gadget: " + event.getGadget() + "is not in the list. Mission name: " + event.getMissionName() + " serial numbers= " + event.getSerialAgentsNumbers());
+					ReleaseAgentsEvent ReleaseAgentsEvent = new ReleaseAgentsEvent(SerialAgentsNumbers);
+					getSimplePublisher().sendEvent(ReleaseAgentsEvent);
+				}
+			}
+			else
+			{
+
 			}
 
 		})  ;
-		// moneypeny should handle the availableAgentEvent
 		subscribeBroadcast(ExpirationBroadcastEvent.class, (broadcast) -> {
 			terminate();
+			System.out.println("M-" + id + " has terminated");
 		});
+
 	}
 }
